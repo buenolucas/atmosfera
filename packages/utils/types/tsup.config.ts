3e0363e5f7fd2d6bc6cfc1e4cfeae53f08c8e411
Lucas Bueno <buenolucasbr@gmail.com>
2022-07-05 13:06:17 -0300

    fix: names

---
+++ packages/utils/types/tsup.config.ts
@@ -0,0 +1,26 @@
+// import isCI from 'is-ci'
+
+import { tsupConfig, TSUpConfigOptions } from '@atmosfera/tsup-config';
+
+const entry = ['src/index.ts'];
+
+/**
+ * @todo(tsup) tsup-node does this automatically we can remove
+ */
+const external = [];
+
+const config: TSUpConfigOptions = {
+  entry,
+  external,
+  minify: true,
+  splitting: true,
+  format: [],
+  tsconfig: 'tsconfig.json',
+};
+
+export default tsupConfig(
+  {
+    ...config,
+  },
+  { arrayMerge: 'overwrite' }
+);
