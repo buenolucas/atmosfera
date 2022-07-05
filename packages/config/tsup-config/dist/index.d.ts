e32c97c0056f879a8fb573a248ef8038fa111863
Lucas Bueno <buenolucasbr@gmail.com>
2022-07-05 12:54:34 -0300

    fix: add dist in gitignor

--- packages/config/tsup-config/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Options } from 'tsup';
-
-declare const defaultConfig: Options;
-declare const tsupConfig: (config?: Options | undefined, deepmergeConfig?: {
-    arrayMerge: "append" | "overwrite";
-} | undefined) => Options;
-
-export { defaultConfig, tsupConfig };
--- packages/core/config-creator/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare const configCreator: <T extends Record<string, any>>(defaultConfig?: T) => (config?: T, deepmergeConfig?: {
-    arrayMerge: 'append' | 'overwrite';
-}) => T;
-
-export { configCreator };
